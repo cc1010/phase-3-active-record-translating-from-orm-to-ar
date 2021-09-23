efad50cac2602da744aa7b72700d487857bafffc
chaim <chaimyisroel@gmail.com>
2021-09-23 18:59:02 -0500

    commit tree 3b3d3d84781fd275914d03220a3ca1dde1a8f960

--- config/environment.rb
+++ config/environment.rb
@@ -10,3 +10,19 @@ ActiveRecord::Base.establish_connection(
   adapter: "sqlite3",
   database: "./db/test.sqlite3"
 )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+# Log SQL output to the terminal
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# Have the Student class inherit from ActiveRecord::Base
+class Dog < ActiveRecord::Base
+end
